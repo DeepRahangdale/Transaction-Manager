f0e336e8a6e37d470f011ae8c7f9bc650bc0575e
DeepRahangdale <deeprahangdale2003@gmai.com>
2024-07-21 19:43:52 +0530

    Update!!

--- src/components/TransactionList.tsx
+++ src/components/TransactionList.tsx
@@ -20,22 +20,34 @@ const TransactionList: React.FC = () => {
   };
 
   return (
-    <div className="p-4">
+    <div className="p-4 flex flex-col items-center">
       <button
         onClick={handleAdd}
         className="bg-blue-500 text-white px-4 py-2 rounded mb-4 transition duration-300 ease-in-out transform hover:bg-blue-700 hover:scale-105 w-full md:w-auto"
       >
         Add Transaction
       </button>
-      <div className="space-y-4">
-        {transactions.map((transaction) => (
-          <TransactionItem
-            key={transaction.id}
-            transaction={transaction}
-            onEdit={() => handleEdit(transaction)}
-          />
-        ))}
-      </div>
+      {transactions.length === 0 ? (
+        <div className="w-full h-full flex flex-col items-center justify-center relative">
+          {/* <div
+            className="absolute top-0 left-0 w-full h-full bg-no-repeat bg-cover bg-center"
+            style={{ backgroundImage: 'url(/path/to/your/image.jpg)' }}
+          ></div>
+          <p className="relative z-10 text-xl text-gray-700 bg-white bg-opacity-75 px-4 py-2 rounded">
+            No transactions to show
+          </p> */}
+        </div>
+      ) : (
+        <div className="space-y-4 w-full">
+          {transactions.map((transaction) => (
+            <TransactionItem
+              key={transaction.id}
+              transaction={transaction}
+              onEdit={() => handleEdit(transaction)}
+            />
+          ))}
+        </div>
+      )}
       {isModalOpen && (
         <AddEditTransactionModal
           transaction={selectedTransaction!}
